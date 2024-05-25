6e7f1bdff28b55baa43c6faaee564e3e25b18fee
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2024-05-25 17:11:26 +0200

    rabbitmq received done

--- src/main/java/publisher/QueueSender.java
+++ src/main/java/publisher/QueueSender.java
@@ -7,17 +7,21 @@ import com.rabbitmq.client.ConnectionFactory;
 public class QueueSender {
 
     private final static String QUEUE_NAME = "hello";
+    //private final static String host = "141.13.5.134";
+    private final static String host = "localhost";
+    //private final static String ROUTING_KEY = "helloMessageQueue";
+    private final static String ROUTING_KEY = QUEUE_NAME;
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
+        factory.setHost(host);
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
 
             channel.queueDeclare(QUEUE_NAME, false,
                     false, false, null);
-            String message = "Hello World!";
-            channel.basicPublish("", QUEUE_NAME,
+            String message = "Hello World 4.0!";
+            channel.basicPublish("", ROUTING_KEY,
                     null, message.getBytes());
             System.out.println(" [x] Sent '" + message + "'");
 
